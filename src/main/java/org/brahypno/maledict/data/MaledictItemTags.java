18f303b58fec72cdcf19fbf20ebf77179880ded8
Brahypno <alayaalsatia19@gmail.com>
2026-09-07 00:59:07 -0400

    scythe tag

--- src/main/java/org/brahypno/maledict/data/MaledictItemTags.java
+++ src/main/java/org/brahypno/maledict/data/MaledictItemTags.java
@@ -22,12 +22,16 @@ public final class MaledictItemTags extends ItemTagsProvider {
     private static final TagKey<Item> GOETY_GRAVE_GLOVE =
             ItemTags.create(new ResourceLocation("goety", "grave_glove_boost"));
 
+    private static final TagKey<Item> FORGE_SCYTHE =
+            ItemTags.create(new ResourceLocation("forge", "scythe"));
+
     public MaledictItemTags(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagsProvider.TagLookup<Block>> blockTags, ExistingFileHelper existingFiles) {
         super(output, lookupProvider, blockTags, Maledict.MODID, existingFiles);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+        tag(FORGE_SCYTHE).add(MaledictItems.INCURSUS_BLADE.get());
         tag(ItemTagRegistry.SCYTHE).add(MaledictItems.INCURSUS_BLADE.get());
         tag(ItemTagRegistry.SOUL_HUNTER_WEAPON).add(MaledictItems.INCURSUS_BLADE.get());
         tag(ItemTagRegistry.MAGIC_CAPABLE_WEAPON).add(MaledictItems.INCURSUS_BLADE.get());
