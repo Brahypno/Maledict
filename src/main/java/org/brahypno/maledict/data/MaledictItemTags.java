ff5bb9e383c955b9ea1184c91f460c383e5ad1b3
Brahypno <alayaalsatia19@gmail.com>
2026-09-10 21:10:52 -0400

    incursus blade no seal

--- src/main/java/org/brahypno/maledict/data/MaledictItemTags.java
+++ src/main/java/org/brahypno/maledict/data/MaledictItemTags.java
@@ -25,6 +25,9 @@ public final class MaledictItemTags extends ItemTagsProvider {
     private static final TagKey<Item> FORGE_SCYTHE =
             ItemTags.create(new ResourceLocation("forge", "scythe"));
 
+    private static final TagKey<Item> L2_NO_SEAL =
+            ItemTags.create(new ResourceLocation("l2hostility", "no_seal"));
+
     public MaledictItemTags(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagsProvider.TagLookup<Block>> blockTags, ExistingFileHelper existingFiles) {
         super(output, lookupProvider, blockTags, Maledict.MODID, existingFiles);
     }
@@ -46,5 +49,6 @@ public final class MaledictItemTags extends ItemTagsProvider {
         tag(ItemTagRegistry.MAGIC_CAPABLE_WEAPON).add(MaledictItems.INCURSUS_BLADE.get());
         tag(ItemTagRegistry.HIDDEN_UNTIL_VOID).add(MaledictItems.INCURSUS_BLADE.get());
         tag(GOETY_GRAVE_GLOVE).add(MaledictItems.INCURSUS_BLADE.get());
+        tag(L2_NO_SEAL).add(MaledictItems.INCURSUS_BLADE.get());
     }
 }
